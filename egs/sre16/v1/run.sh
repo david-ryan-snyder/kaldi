22d89ab530a0bd0d23fbd1c42690d8c3593e0cbc
David Snyder <dsnyder@b01.clsp.jhu.edu>
2017-06-01 18:29:55 -0400

    [egs] updating results in egs/sre10/v1/run.sh

--- egs/sre16/v1/run.sh
+++ egs/sre16/v1/run.sh
@@ -191,15 +191,15 @@ $train_cmd exp/scores/log/sre16_eval_scoring.log \
 echo "modelid    segment    side    LLR" > exp/scores/converted_sre16_eval_scores_snorm
 awk '{print $1, $2, "a", $3}' exp/scores/sre16_eval_scores_snorm >> exp/scores/converted_sre16_eval_scores_snorm
 
-# EER 11.12, DCF16 0.653
+# EER 11.06, DCF16 0.650
 python3 ${scoring_dir}/scoring.py \
   ${scoring_dir}/eval_config.ini -o exp/scores/converted_sre16_eval_scores_snorm 2>&1 | tee exp/scores/pool_results_snorm
 
-# EER 6.99, DCF16 0.509
+# EER 6.95, DCF16 0.508
 python3 ${scoring_dir}/scoring.py \
   ${scoring_dir}/eval_yue_config.ini -o exp/scores/converted_sre16_eval_scores_snorm 2>&1 | tee exp/scores/yue_results_snorm
 
-# EER 15.04, DCF16 0.790
+# EER 14.88, DCF16 0.785
 python3 ${scoring_dir}/scoring.py \
   ${scoring_dir}/eval_tgl_config.ini -o exp/scores/converted_sre16_eval_scores_snorm 2>&1 | tee exp/scores/tgl_results_snorm
 
