06117fb4cdfd656eb1886dde7b7d8e42bff86c4b
David Snyder <dsnyder@Davids-MacBook-Air-2.local>
2018-09-22 22:31:10 -0400

    changing voice search functionality

--- egs/voxceleb/gbo_demo/get_voice_search_output.py
+++ egs/voxceleb/gbo_demo/get_voice_search_output.py
@@ -11,5 +11,6 @@ scores = sorted(scores, key = lambda tup: tup[2], reverse=True)
 out_str = ""
 for x in scores:
   spkr, utt, score = x
-  out_str = out_str + spkr + ":" + str(score) + ";"
+  #out_str = out_str + spkr + ":" + str(score) + ";"
+  out_str = out_str + spkr + ";"
 print out_str
