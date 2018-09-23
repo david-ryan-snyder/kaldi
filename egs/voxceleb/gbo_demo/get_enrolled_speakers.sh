726a40cebd20b2761d79a79bae2d15bc6f8a1345
David Snyder <dsnyder@Davids-MacBook-Air-2.local>
2018-09-23 01:05:01 -0400

    get enrolled speakers sorts before returning

--- egs/voxceleb/gbo_demo/get_enrolled_speakers.sh
+++ egs/voxceleb/gbo_demo/get_enrolled_speakers.sh
@@ -2,4 +2,4 @@
 . ./cmd.sh
 . ./path.sh
 nnet_dir=exp/xvector_dgr5
-awk '{print $1}' $nnet_dir/xvectors_enrollments/spk_xvector.scp | tr '\n' ';'
+awk '{print $1}' $nnet_dir/xvectors_enrollments/spk_xvector.scp | sort | tr '\n' ';'
